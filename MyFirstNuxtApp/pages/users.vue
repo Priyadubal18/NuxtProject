43c7a776687f3f26229f667c8011fbdb1a259c81
Priyadubal18 <priya.dubal18@gmai.com>
2019-12-14 20:38:12 -0500

    Added the nust file

---
+++ MyFirstNuxtApp/pages/users.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>{{welcomeMessage}}</h1>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      welcomeMessage: "Welcome to  user page"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
